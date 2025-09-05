f6c152098a9bbc6eccec7eebb2ff908ccc48be07
TeacherTalley <ptalley2@uca.edu>
2025-09-05 11:31:35 -0500

    Changing unit tests in bash script

--- test_cases.bash
+++ test_cases.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This script runs the test cases for the dog module using unittest and pytest.
-python -m unittest dog test_dog.TestDog.test_dog_initialization
+python -m unittest dog test_dog.TestDog.test_bark_method
+python -m unittest dog test_dog.TestDog.test_sit_method
 # Run all tests in the dog module
 python -m pytest
